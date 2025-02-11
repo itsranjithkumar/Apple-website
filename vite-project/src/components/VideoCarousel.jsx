93b837a3664e9e408a9cca065799827d1c87e567
itsranjithkumar <163500531+itsranjithkumar@users.noreply.github.com>
2025-02-11 10:14:57 +0530

    changes

--- vite-project/src/components/VideoCarousel.jsx
+++ vite-project/src/components/VideoCarousel.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react'
 import { hightlightsSlides } from "../constants";
+import { playImg } from '../utils';
 
 const VideoCarousel = () => {
     const videoRef = useRef([]);
@@ -14,7 +15,20 @@ const VideoCarousel = () => {
         isPlaying: false,
     });
 
+    const [loadedData, setLoadedData] = useState([]);
+
     const { isEnd, isStartPlay, videoId, isLastVideo, isPlaying } = video;
+
+    useEffect (() => {
+        
+        if (loadedData.length > 3) {
+            if(!isPlaying) {
+                videoRef.current[videoId].pause();
+            } else {
+                startPlay && videoRef.current[videoId].play();
+            }
+        }
+    },[isPlaying, loadedData])
 
     useEffect(() => {
         let currentProgress = 0;
@@ -29,37 +43,66 @@ const VideoCarousel = () => {
     }, [videoId, isStartPlay]);
 
     return (
-        <div className="flex items-center">
-            {hightlightsSlides.map((list, i) => (
-                <div key={list.id} id="slider"
-                 className="sm:pr-20 pr-10">
-                    <div className="video-carousel_container">
-                        <div className="w-full h-full flex-center
-                        rounded-3xl overflow-hidden bg-black">
-                            <video 
-                                ref={(el) => videoRef.current[i] = el}
-                                id="video"
-                                playsInline={true}
-                                muted
-                                preload="auto"
-                                // Add onEnded and onClick handlers if needed
-                                // onEnded={() => handleVideoEnd()}
-                                // onClick={() => handleVideoClick(i)}
-                            >
-                                <source src={list.video} type='video/mp4' />
-                            </video>
-                        </div>
+        <>
+            <div className="flex items-center">
+                {hightlightsSlides.map((list, i) => (
+                    <div key={list.id} id="slider"
+                     className="sm:pr-20 pr-10">
+                        <div className="video-carousel_container">
+                            <div className="w-full h-full flex-center
+                            rounded-3xl overflow-hidden bg-black">
+                                <video 
+                                    ref={(el) => (videoRef.current[i] = el)}
+                                    id="video"
+                                    playsInline={true}
+                                    muted
+                                    preload="auto"
+                                    onPlay={() => {
+                                        setVideo((prevVideo) => ({
+                                            ...prevVideo,
+                                            isPlaying: true
+                                        }))
+                                    }} 
+                                    // Add onEnded and onClick handlers if needed
+                                    // onEnded={() => handleVideoEnd()}
+                                    // onClick={() => handleVideoClick(i)}
+                                >
+                                    <source src={list.video} type='video/mp4' />
+                                </video>
+                            </div>
 
-                        <div className="absolute top-12 left-[5%]
-                        z-10">
-                            {list.textLists.map((text) => (
-                                <p key={text} className="md:text-2xl text-xl font-medium">{text}</p>
-                            ))}
+                            <div className="absolute top-12 left-[5%]
+                            z-10">
+                                {list.textLists.map((text) => (
+                                    <p key={text} className="md:text-2xl text-xl font-medium">{text}</p>
+                                ))}
+                            </div>
                         </div>
                     </div>
+                ))}
+            </div>
+
+            <div className="relative flex-center mt-10">
+                <div className="flex-center py-5 px-7
+                    bg-gray-300 backdrop-blur rounded-full">
+                        {videoRef.current.map((_,i) => (
+                            <span key={i}
+                                ref={(el) => (videoDivRef.current[i] = el)}
+                                className="mx-2 w-3 h-3 bg-gray-300
+                                rounded-full relative cursor-pointer"
+                                >
+                                <span className="absolute h-full w-full bg-white rounded-full opacity-50" ref={(el) => (videoSpanRef.current[i] = el)} ></span>
+                            </span>))}
                 </div>
-            ))}
-        </div>
+
+                <button className="control-btn">
+                    <img
+                    src={isLastVideo ? replayImg : !isPlaying ? playImg : pauseImg}
+                    />
+                </button>
+                
+            </div>
+        </>
     )
 }
 
